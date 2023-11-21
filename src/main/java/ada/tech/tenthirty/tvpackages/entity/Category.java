1302778336f68fd96dc17d4c1f8cca782cba5026
Renara Secchim <renarasecchim@gmail.com>
2023-11-21 10:14:10 -0300

    feat: create entity

---
+++ src/main/java/ada/tech/tenthirty/tvpackages/entity/Category.java
@@ -0,0 +1,8 @@
+package ada.tech.tenthirty.tvpackages.entity;
+
+public enum Category {
+  SPORT,
+  MOVIES,
+  MUSIC,
+  ADVENTURE,
+}
---
+++ src/main/java/ada/tech/tenthirty/tvpackages/entity/Package.java
@@ -0,0 +1,24 @@
+package ada.tech.tenthirty.tvpackages.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Package {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+  private String skuId;
+  private String name; 
+  private List<Channel> listChannels;  
+  private BigDecimal value;
+}
