4e57fa7a626eb7ca1dbd116cc6a6120bf446e200
Renara Secchim <renarasecchim@gmail.com>
2023-11-22 12:34:34 -0300

    chore:resolve conflit rebase develop

---
+++ src/main/java/ada/tech/tenthirty/tvpackages/entity/Channel.java
@@ -0,0 +1,24 @@
+package ada.tech.tenthirty.tvpackages.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Channel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+  private String skuId;
+  private Integer identifier;
+  private String name;
+  private Category category;
+}
